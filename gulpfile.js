978fb5bdc81328e9b0a85793c6763551a5247c86
Scott <scott_tat@yahoo.com>
2016-12-12 21:01:46 -0600

    Install postcss imports, imported normalize.css, overlay text on background

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,20 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
 
 
 gulp.task('default', function(){
-  console.log('Working');
+  
 });
 
 gulp.task('html', function(){
-  console.log('Something happens');
+
 });
 
 gulp.task('styles', function(){
  return  gulp.src('./app/assets/styles/styles.css')
- .pipe(postcss([cssvars,nested, autoprefixer]))
+ .pipe(postcss([cssImport , cssvars , nested, autoprefixer]))
  .pipe(gulp.dest('./app/temp/styles'));
 });
 
